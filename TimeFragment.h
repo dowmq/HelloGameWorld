0afec0ed0c042dd68de5a12cc777a760b283825e
dowmq <49703786+dowmq@users.noreply.github.com>
2019-06-24 15:16:08 +0900

    Update TimeFragment.h

--- TimeFragment.h
+++ TimeFragment.h
@@ -8,12 +8,12 @@ class TimeFragment
 public:
 	TimeFragment() : time{ steady_clock::now() } {}
 	TimeFragment(const TimeFragment& before) : time{ before.time } {}
-	TimeFragment& operator=(const TimeFragment& before) { time = before.time; }
+	TimeFragment& operator=(const TimeFragment& before) { time = before.time; return *this; }
 	TimeFragment& operator=(TimeFragment&& before) { time = std::move(before.time); return *this; }
 
 	void		Update() { time = steady_clock::now(); }
 	double		operator-(TimeFragment& before) {
-					return (time - before.time).count();
+		return std::chrono::duration_cast<std::chrono::seconds>(time - before.time).count();
 	}
 public:
 	std::chrono::time_point<steady_clock> time;
